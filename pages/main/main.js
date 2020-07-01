7f5e5e74d8c2fdf108e0abe05f6f9d7a7ead886c
Tibbar <851207725@qq.com>
2020-07-01 08:48:47 +0800

    small changes

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,7 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+        type: 'image',
+        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }],
   },
 
   /**
