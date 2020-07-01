7a1427abd5fed2759a6ed40be5b55bb15a4e45e8
Tibbar <851207725@qq.com>
2020-07-01 14:31:52 +0800

    job merged

--- pages/job-detail/job-detail.js
+++ pages/job-detail/job-detail.js
@@ -1,1 +1,22 @@
-Page({})
+Page({
+  //事件处理函数
+  bindTapHome: function() {
+    // TODO
+    this.onLoad();
+  },
+  bindTapNews: function() {
+    wx.redirectTo({
+      url: '../Jobs/jobs',
+    })
+  },
+  bindTapFile: function() {
+    wx.redirectTo({
+      url: '../profile/profile',
+    })
+  },
+  bindTapMy: function() {
+    wx.redirectTo({
+      url: '../my/my',
+    })
+  }
+})
