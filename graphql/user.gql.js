a2e22fec70521de98497fdece557410603563e2e
Tibbar <851207725@qq.com>
2020-07-03 15:24:23 +0800

    merged

---
+++ graphql/user.gql.js
@@ -0,0 +1,50 @@
+const gql = require('./common');
+
+const GET_USER_INFO_BY_ID = `
+query ($uid: Int!){
+  getUserInfoById(userId: $uid){
+    id
+    openid
+    name
+    birthday
+    education
+    province
+    city
+    region
+    avatar_url
+    email
+    phone
+  }
+}
+`
+
+const UPDATE_USER_INFO = `
+mutation ($userInput: UserInfoUpdateInput!){
+  updateUserInfo(userInput: $userInput)
+}
+`
+
+
+const getUserInfoById = function(userId){
+  return gql.getGqlObject().query({
+    query: GET_USER_INFO_BY_ID,
+    varibles: {
+      userId: +userId
+    }
+  });
+}
+
+const updateUserInfo = function(userInput){
+  console.log(userInput);
+  return gql.getGqlObject().mutate({
+    mutate: UPDATE_USER_INFO,
+    varibles: {
+      userInput: userInput
+    }
+  });
+}
+
+module.exports = {
+  getUserInfoById: getUserInfoById,
+  updateUserInfo: updateUserInfo
+}
