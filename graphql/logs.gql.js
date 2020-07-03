147b8e23a5017c5033eb16013b09e626e89314ec
unknown <unknown@example.com>
2020-07-03 17:00:31 +0800

    merge into dev

--- graphql/logs.gql.js
+++ graphql/logs.gql.js
@@ -6,6 +6,25 @@ query ($id: Int!) {
 }
 `;
 
+const BIND_OPENID = `
+query($openId: String!){
+  bindOpenId(openId:$openId)
+  {
+    id
+  }
+}
+`
+const bindOpenId = function(openId) {
+  return gql.getGqlObject().query({
+    query:  BIND_OPENID,
+    variables: {
+      openId: openId
+    }
+  });
+}
+console.log(bindOpenId("sa"));
+
+
 const gqlLogsTest = function () {
   return gql.getGqlObject().query({
     query: GET_NAME,
