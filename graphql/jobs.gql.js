b2411fbd5044bebf73752dda0f3f8b13a34948a7
unknown <unknown@example.com>
2020-07-04 15:38:58 +0800

    搜索和筛选

--- graphql/jobs.gql.js
+++ graphql/jobs.gql.js
@@ -32,6 +32,22 @@ query ($positionId: Int!) {
 }
 `;
 
+const SEARCH_FILTERED_POSITIONS = `
+query ($skip: Int, $take: Int, $filterInput: PositionFilterInput, $searchInput: String) {
+	searchFilteredPositions(skip: $skip, take: $take, filterInput: $filterInput, searchInput: $searchInput) {
+    list {
+      id
+      company_id
+      name
+      profile
+      condition
+      created_at
+      updated_at
+    }
+  }
+}
+`;
+
 const gqlDefaultJobsList = function () {
   return gql.getGqlObject().query({
     query: GET_DEFAULT_POSITIONS,
@@ -47,7 +63,20 @@ const getJobById = function(jobId) {
   });
 }
 
+const searchFilteredPositions = function(searchInput, filterInput, skip, take) {
+  return gql.getGqlObject().query({
+    query: SEARCH_FILTERED_POSITIONS,
+    variables: {
+      searchInput: searchInput,
+      filterInput: filterInput,
+      skip: skip,
+      take: take
+    }
+  });
+}
+
 module.exports = {
   gqlDefaultJobsList: gqlDefaultJobsList,
-  getJobById: getJobById
+  getJobById: getJobById,
+  searchFilteredPositions: searchFilteredPositions
 }
